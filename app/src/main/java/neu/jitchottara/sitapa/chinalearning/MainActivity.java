f98e4f85eb94fe20df719cac463baa8e37c51931
sitapa <sri.sitapa@gmail.com>
2016-03-03 10:31:57 +0700

    Add Value & Test finish

--- app/src/main/java/neu/jitchottara/sitapa/chinalearning/MainActivity.java
+++ app/src/main/java/neu/jitchottara/sitapa/chinalearning/MainActivity.java
@@ -16,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         //Request Database เป็นตัว run ครั้งแรก
         myManage = new MyManage(this);
 
+        //Test Add Value ทดสอบการ test  value
+        testAddValue();
+
     } // Main Method
+
+    private void testAddValue() {
+        myManage.addLearn("testUnit", "ง่าย", "urlImage", "二 ", "read", "meaning", "urlSound");
+    }
 } // Main Class
--- app/src/main/java/neu/jitchottara/sitapa/chinalearning/MyManage.java
+++ app/src/main/java/neu/jitchottara/sitapa/chinalearning/MyManage.java
@@ -1,5 +1,6 @@
 package neu.jitchottara.sitapa.chinalearning;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,16 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String learmn_table = "learnTABLE";
+    public static final String column_Unit = "Unit";
+    public static final String column_Level = "Level";
+    public static final String column_Image = "Image";
+    public static final String column_Vocabulary = "Vocabulary";
+    public static final String column_Read = "Read";
+    public static final String column_Meaning = "Meaning";
+    public static final String column_Sound = "Sound";
+
+
     public MyManage(Context context) {
 
         //Create& Connected database
@@ -20,4 +31,24 @@ public class MyManage {
         readSqLiteDatabase = myOpenHelper.getReadableDatabase();
 
     } //Constructor
+    public long addLearn(String strUnit,
+                         String strLevel,
+                         String strImage,
+                         String strVocabulary,
+                         String strRead,
+                         String strMeaning,
+                         String strSound) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_Unit,strUnit);
+        contentValues.put(column_Level,strLevel);
+        contentValues.put(column_Image,strImage);
+        contentValues.put(column_Vocabulary,strVocabulary);
+        contentValues.put(column_Read,strRead);
+        contentValues.put(column_Meaning,strMeaning);
+        contentValues.put(column_Sound,strSound);
+
+        return writeSqLiteDatabase.insert(learmn_table,null,contentValues);
+    }
+
 } //Main Class
