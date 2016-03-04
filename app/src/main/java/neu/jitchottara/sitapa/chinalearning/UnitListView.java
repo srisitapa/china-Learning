ee32dbced0d2d92241407f7663c6283dda068c12
sitapa <sri.sitapa@gmail.com>
2016-03-04 10:14:51 +0700

    Intent & putExtra to Detail finish

--- app/src/main/java/neu/jitchottara/sitapa/chinalearning/UnitListView.java
+++ app/src/main/java/neu/jitchottara/sitapa/chinalearning/UnitListView.java
@@ -1,9 +1,12 @@
 package neu.jitchottara.sitapa.chinalearning;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -43,13 +46,18 @@ public class UnitListView extends AppCompatActivity {
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM learnTABLE WHERE Unit = " + "'" + unitString + "'" + " AND Level = " + "'" + levelString + "'", null);
         cursor.moveToFirst();
 
-        String[] iconStrings = new String[cursor.getCount()];
-        String[] vocabularyStrings = new String[cursor.getCount()];
+        final String[] iconStrings = new String[cursor.getCount()];
+        final String[] vocabularyStrings = new String[cursor.getCount()];
+        final String[] readStrings = new String[cursor.getCount()];
+        final String[] meaningStrings = new String[cursor.getCount()];
+        final String[] soundStrings = new String[cursor.getCount()];
 
         for (int i = 0; i < cursor.getCount(); i++) {
             iconStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Image));
             vocabularyStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Vocabulary));
-
+            readStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Read));
+            meaningStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Meaning));
+            soundStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Sound));
 
             cursor.moveToNext();
         }   //for
@@ -57,6 +65,22 @@ public class UnitListView extends AppCompatActivity {
 
         UnitAdapter unitAdapter = new UnitAdapter(UnitListView.this, iconStrings, vocabularyStrings);
         detailListView.setAdapter(unitAdapter);
+
+        detailListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+                Intent intent = new Intent(UnitListView.this, DetailActivity.class);
+                intent.putExtra("Title", showTitle.getText().toString());
+                intent.putExtra("urlImage", iconStrings[i]);
+                intent.putExtra("Vocabulary", vocabularyStrings[i]);
+                intent.putExtra("Read", readStrings[i]);
+                intent.putExtra("Meaning", meaningStrings[i]);
+                intent.putExtra("urlSound", soundStrings[i]);
+                startActivity(intent);
+
+            }   //event
+        });
 
     }   //Create ListView
 
